c7d333bdcaa96d15be198315a1dfbfbbe3e5f730
Abdelwahab Sadek <abdelwahab.sadek@coolblue.nl>
2019-12-06 15:44:33 +0100

    Initialize the repo with benchmark package

---
+++ BenchmarkIntro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BenchmarkIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Benchmark!");
+            Console.ReadLine();
+        }
+    }
+}
